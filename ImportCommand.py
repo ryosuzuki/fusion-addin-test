688e2156c5543cf9fd05795c8707cb704ae7597c
Ryo Suzuki <ryosuzk@gmail.com>
2017-09-03 03:01:52 -0600

    Occurence

--- ImportCommand.py
+++ ImportCommand.py
@@ -94,6 +94,103 @@ def getSelectedObjects(selectionInput):
       objects.append(selectedObj)
   return objects
 
+class ImportCommand(Fusion360CommandBase.Fusion360CommandBase):
+  def onPreview(self, command, inputs):
+    pass
+
+  def onDestroy(self, command, inputs, reason_):
+    pass
+
+  def onInputChanged(self, command, inputs, changedInput):
+    if command.commandInput.id == command.parentCommandDefinition.id + '_project':
+
+      projectName = command.commandInputs.itemById(command.parentCommandDefinition.id + '_project').selectedItem.name
+      fillFilesDictionary(projectName)
+      currentProject = projectFiles[projectName]
+      fileInput = command.commandInputs.itemById(command.parentCommandDefinition.id + '_file')
+      addItemsToDropdown(currentProject, fileInput)
+
+  def onCreate(self, command, inputs):
+    app = adsk.core.Application.get()
+    ui  = app.userInterface
+    global projects
+    projects = app.data.dataProjects
+
+    fillProjectsDictionary()
+    dropdownInput = inputs.addDropDownCommandInput(command.parentCommandDefinition.id + '_project', 'Project', adsk.core.DropDownStyles.TextListDropDownStyle)
+    projectItem = addItemsToDropdown(projectFiles, dropdownInput)
+
+    fillFilesDictionary(projectItem.name)
+    dropdownInput = inputs.addDropDownCommandInput(command.parentCommandDefinition.id + '_file', 'File', adsk.core.DropDownStyles.TextListDropDownStyle)
+    project = projectFiles[projectItem.name]
+    addItemsToDropdown(project, dropdownInput)
+
+
+  def onExecute(self, command, inputs):
+    projectName = command.commandInputs.itemById(command.parentCommandDefinition.id + '_project').selectedItem.name
+    fileName = command.commandInputs.itemById(command.parentCommandDefinition.id + '_file').selectedItem.name
+    file = getFile(projectName, fileName)
+
+    app = adsk.core.Application.get()
+    design = app.activeProduct
+    rootComp = design.rootComponent
+    occs = rootComp.occurrences
+
+    matrix = adsk.core.Matrix3D.create()
+    occ = occs.addByInsert(file, matrix, False)
+    materialLib = app.materialLibraries.itemByName("Fusion 360 Appearance Library")
+    appearance = materialLib.appearances.itemByName("Plastic - Matte (Yellow)")
+    occ.appearance = appearance
+
+    sketches = rootComp.sketches
+    sketch = sketches.add(rootComp.xZConstructionPlane)
+    sketchPts = sketch.sketchPoints
+    point = adsk.core.Point3D.create(1, 0, 1)
+    sketchPt = sketchPts.add(point)
+    sketchCircles = sketch.sketchCurves.sketchCircles
+    centerPoint = adsk.core.Point3D.create(0, 0, 0)
+    circle = sketchCircles.addByCenterRadius(centerPoint, 5.0)
+
+    # Get the profile defined by the circle
+    prof = sketch.profiles.item(0)
+
+    # Create an extrusion input and make sure it's in a new component
+    extrudes = rootComp.features.extrudeFeatures
+    extInput = extrudes.createInput(prof, adsk.fusion.FeatureOperations.NewComponentFeatureOperation)
+
+    # Set the extrusion input
+    distance = adsk.core.ValueInput.createByReal(5)
+    extInput.setDistanceExtent(True, distance)
+    extInput.isSolid = True
+
+    # Create the extrusion
+    ext = extrudes.add(extInput)
+
+    # Get the end face of the created extrusion body
+    endFace = ext.endFaces.item(0)
+
+    sketchInOcc = sketches.add(endFace, occ)
+    curve = sketchInOcc.sketchCurves.item(0)
+
+    geo0 = adsk.fusion.JointGeometry.createByCurve(curve, adsk.fusion.JointKeyPointTypes.CenterKeyPoint)
+
+    # Create the second joint geometry with sketch point
+    geo1 = adsk.fusion.JointGeometry.createByPoint(sketchPt)
+
+
+    joints = occ.component.joints
+    jointInput = joints.createInput(geo0, geo1)
+    jointInput.setAsPlanarJointMotion(adsk.fusion.JointDirections.YAxisJointDirection)
+    joint = joints.add(jointInput)
+
+    # planarMotion = joint.jointMotion
+    # limits = planarMotion.rotationLimits
+    # limits.isRestValueEnabled = True
+    # limits.restValue = 1.0
+
+    return
+
+
 def draw(selectedPlane, selectedFile):
   app = adsk.core.Application.get()
   ui  = app.userInterface
@@ -147,93 +244,4 @@ def componentsFromBodies(items):
     components.append(component)
   return components
 
-
-class ImportCommand(Fusion360CommandBase.Fusion360CommandBase):
-  def onPreview(self, command, inputs):
-    pass
-
-  def onDestroy(self, command, inputs, reason_):
-    pass
-
-  def onInputChanged(self, command, inputs, changedInput):
-    if command.commandInput.id == command.parentCommandDefinition.id + '_project':
-
-      projectName = command.commandInputs.itemById(command.parentCommandDefinition.id + '_project').selectedItem.name
-      fillFilesDictionary(projectName)
-      currentProject = projectFiles[projectName]
-      fileInput = command.commandInputs.itemById(command.parentCommandDefinition.id + '_file')
-      addItemsToDropdown(currentProject, fileInput)
-
-  def onCreate(self, command, inputs):
-    app = adsk.core.Application.get()
-    ui  = app.userInterface
-    global projects
-    projects = app.data.dataProjects
-
-    fillProjectsDictionary()
-    dropdownInput = inputs.addDropDownCommandInput(command.parentCommandDefinition.id + '_project', 'Project', adsk.core.DropDownStyles.TextListDropDownStyle)
-    projectItem = addItemsToDropdown(projectFiles, dropdownInput)
-
-    fillFilesDictionary(projectItem.name)
-    dropdownInput = inputs.addDropDownCommandInput(command.parentCommandDefinition.id + '_file', 'File', adsk.core.DropDownStyles.TextListDropDownStyle)
-    project = projectFiles[projectItem.name]
-    addItemsToDropdown(project, dropdownInput)
-
-
-  def onExecute(self, command, inputs):
-    projectName = command.commandInputs.itemById(command.parentCommandDefinition.id + '_project').selectedItem.name
-    fileName = command.commandInputs.itemById(command.parentCommandDefinition.id + '_file').selectedItem.name
-    file = getFile(projectName, fileName)
-
-    app = adsk.core.Application.get()
-    design = app.activeProduct
-    rootComp = design.rootComponent
-    occs = rootComp.occurrences
-    component = occs.addByInsert(file, adsk.core.Matrix3D.create(), True)
-
-    materialLib = app.materialLibraries.itemByName("Fusion 360 Appearance Library")
-    appearance = materialLib.appearances.itemByName("Plastic - Matte (Yellow)") # "Paint - Enamel Glossy (Yellow)"
-
-
-    component.appearance = appearance
-
-    return
-
-
-    # plane = getInputs(command, inputs)
-    # filepath = selectFile()
-    # if (plane):
-    #   draw(plane, filepath)
-
-    # # return
-
-
-    # app = adsk.core.Application.get()
-    # ui  = app.userInterface
-    # product = app.activeProduct
-    # design = adsk.fusion.Design.cast(product)
-    # rootComp = design.rootComponent
-
-    # targetBody = rootComp.bRepBodies.item(0)
-    # spacing = 10
-    # quantity = 10
-    # plane = targetBody.parentComponent.xZConstructionPlane
-    # thickness = 1
-
-    # results = slice(targetBody, spacing, quantity, plane, thickness)
-
-    # components = componentsFromBodies(results)
-
-    # # futil.combine_feature(point[1], tool_bodies, adsk.fusion.FeatureOperations.CutFeatureOperation)
-
-    # combineFeatures = targetBody.parentComponent.features.combineFeatures
-
-    # combineTools = adsk.core.ObjectCollection.create()
-    # for tool in tool_bodies:
-    #   combineTools.add(tool)
-
-    # # Create Combine Feature
-    # combine_input = combine_features.createInput(target_body, combine_tools)
-    # combine_input.operation = operation
-    # combine_features.add(combine_input)
 
