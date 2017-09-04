73771226afd2841ce1df6f013dcb1926c6572872
Ryo Suzuki <ryosuzk@gmail.com>
2017-09-04 14:44:38 -0600

    Enable to select face and add sketch onto the surface

--- ImportCommand.py
+++ ImportCommand.py
@@ -1,45 +1,21 @@
 import adsk.core, adsk.fusion, adsk.cam, traceback
 from . import Fusion360CommandBase
 
-def getSelectedObjects(selectionInput):
-  objects = []
-  for i in range(0, selectionInput.selectionCount):
-    selection = selectionInput.selection(i)
-    selectedObj = selection.entity
-    if adsk.fusion.BRepBody.cast(selectedObj) or \
-      adsk.fusion.BRepFace.cast(selectedObj) or \
-      adsk.fusion.Occurrence.cast(selectedObj):
-      objects.append(selectedObj)
-  return objects
-
-
 class ImportCommand(Fusion360CommandBase.Fusion360CommandBase):
   def onPreview(self, command, inputs):
-    return
-    self.ui.messageBox("preview")
-    self.project = self.projects[2]
-    self.projectFiles[self.project.name] = None
-    dropdownInput = inputs.addDropDownCommandInput(command.parentCommandDefinition.id + '_project', 'Project', adsk.core.DropDownStyles.TextListDropDownStyle)
-    addItemsToDropdown(self.projectFiles, dropdownInput)
-
-    self.files = self.projectFiles[self.project.name]
-    if not self.files:
-      self.dataFiles = self.project.rootFolder.dataFiles
-      self.files = {}
-      for file in self.dataFiles:
-        self.files[file.name] = file
-
-    dropdownInput = inputs.addDropDownCommandInput(command.parentCommandDefinition.id + '_file', 'File', adsk.core.DropDownStyles.TextListDropDownStyle)
-    addItemsToDropdown(self.files, dropdownInput)
+    pass
 
   def onDestroy(self, command, inputs, reason_):
     pass
 
   def onInputChanged(self, command, inputs, changedInput):
-    targetId = command.parentCommandDefinition.id + '_file'
-    if changedInput.id == targetId:
-      name = command.commandInputs.itemById(targetId).selectedItem.name
+    if changedInput.id == self.fileCommandId:
+      name = command.commandInputs.itemById(self.fileCommandId).selectedItem.name
       self.file = self.files[name]
+
+    if changedInput.id == self.planeCommandId:
+      selection = changedInput.selection(0)
+      self.plane = selection.entity
 
   def onCreate(self, command, inputs):
     self.project = None
@@ -48,76 +24,115 @@ class ImportCommand(Fusion360CommandBase.Fusion360CommandBase):
     self.projectNames = {}
     self.fileNames = {}
 
+    self.planeCommandId = command.parentCommandDefinition.id + '_plane'
+    self.projectCommandId = command.parentCommandDefinition.id + '_project'
+    self.fileCommandId = command.parentCommandDefinition.id + '_file'
+
     self.app = adsk.core.Application.get()
     self.ui  = self.app.userInterface
+
+    selectionPlaneInput = inputs.addSelectionInput(self.planeCommandId, 'Select Base Face', 'Select Face to mate to')
+    selectionPlaneInput.setSelectionLimits(1,1)
+    selectionPlaneInput.addSelectionFilter('PlanarFaces')
+
     self.projects = self.app.data.dataProjects
     self.project = self.projects[2]
     self.files = self.project.rootFolder.dataFiles
 
     self.projectNames[self.project.name] = self.project
-    dropdownInput = inputs.addDropDownCommandInput(command.parentCommandDefinition.id + '_project', 'Project', adsk.core.DropDownStyles.TextListDropDownStyle)
+    dropdownInput = inputs.addDropDownCommandInput(self.projectCommandId, 'Project', adsk.core.DropDownStyles.TextListDropDownStyle)
     addItemsToDropdown(self.projectNames, dropdownInput)
 
     self.fileNames = {}
-    self.file = self.files[2]
+    self.file = self.files[3]
     self.fileNames[self.file.name] = self.file
     # for file in self.files:
-      # self.fileNames[file.name] = None
+    #   self.fileNames[file.name] = file
 
-    dropdownInput = inputs.addDropDownCommandInput(command.parentCommandDefinition.id + '_file', 'File', adsk.core.DropDownStyles.TextListDropDownStyle)
+    dropdownInput = inputs.addDropDownCommandInput(self.fileCommandId, 'File', adsk.core.DropDownStyles.TextListDropDownStyle)
     addItemsToDropdown(self.fileNames , dropdownInput)
+
 
   def onExecute(self, command, inputs):
     design = self.app.activeProduct
     rootComp = design.rootComponent
     occs = rootComp.occurrences
 
+    planes = rootComp.constructionPlanes
+    planeInput = planes.createInput()
+    offset = adsk.core.ValueInput.createByReal(0)
+    planeInput.setByOffset(self.plane, offset)
+    plane = planes.add(planeInput)
+    translate = plane.transform.translation
+
     matrix = adsk.core.Matrix3D.create()
+    # distance = 1
+    matrix.translation = adsk.core.Vector3D.create(0, translate.y, 0)
+
     occ = occs.addByInsert(self.file, matrix, False)
-    materialLib = app.materialLibraries.itemByName("Fusion 360 Appearance Library")
+    materialLib = self.app.materialLibraries.itemByName("Fusion 360 Appearance Library")
     appearance = materialLib.appearances.itemByName("Plastic - Matte (Yellow)")
     occ.appearance = appearance
 
-    sketches = rootComp.sketches
-    sketch = sketches.add(rootComp.xZConstructionPlane)
-    sketchPts = sketch.sketchPoints
-    point = adsk.core.Point3D.create(1, 0, 1)
-    sketchPt = sketchPts.add(point)
-    sketchCircles = sketch.sketchCurves.sketchCircles
-    centerPoint = adsk.core.Point3D.create(0, 0, 0)
-    circle = sketchCircles.addByCenterRadius(centerPoint, 5.0)
-
-    # Get the profile defined by the circle
-    prof = sketch.profiles.item(0)
-
-    # Create an extrusion input and make sure it's in a new component
-    extrudes = rootComp.features.extrudeFeatures
-    extInput = extrudes.createInput(prof, adsk.fusion.FeatureOperations.NewComponentFeatureOperation)
-
-    # Set the extrusion input
-    distance = adsk.core.ValueInput.createByReal(5)
-    extInput.setDistanceExtent(True, distance)
-    extInput.isSolid = True
-
-    # Create the extrusion
-    ext = extrudes.add(extInput)
-
-    # Get the end face of the created extrusion body
-    endFace = ext.endFaces.item(0)
-
-    sketchInOcc = sketches.add(endFace, occ)
-    curve = sketchInOcc.sketchCurves.item(0)
-
-    geo0 = adsk.fusion.JointGeometry.createByCurve(curve, adsk.fusion.JointKeyPointTypes.CenterKeyPoint)
-
-    # Create the second joint geometry with sketch point
-    geo1 = adsk.fusion.JointGeometry.createByPoint(sketchPt)
+    # sketches = rootComp.sketches
 
 
-    joints = occ.component.joints
-    jointInput = joints.createInput(geo0, geo1)
-    jointInput.setAsPlanarJointMotion(adsk.fusion.JointDirections.YAxisJointDirection)
-    joint = joints.add(jointInput)
+    # component = occ.component
+    # face = component.xZConstructionPlane
+    # # body = component.bRepBodies.item(0)
+    # # face = body.faces[0]
+
+
+    # geo0 = adsk.fusion.JointGeometry.createByPlanarFace(self.plane, None, adsk.fusion.JointKeyPointTypes.CenterKeyPoint)
+    # geo1 = adsk.fusion.JointGeometry.createByPlanarFace(face, None, adsk.fusion.JointKeyPointTypes.CenterKeyPoint)
+
+    # joints = occ.component.joints
+    # jointInput = joints.createInput(geo0, geo1)
+    # jointInput.setAsPlanarJointMotion(adsk.fusion.JointDirections.YAxisJointDirection)
+    # joint = joints.add(jointInput)
+
+    # sketchInOcc = sketches.add(plane)
+    # curve = sketchInOcc.sketchCurves.item(0)
+
+    # sketch = sketches.add(planeOne)
+    # sketchPts = sketch.sketchPoints
+    # point = adsk.core.Point3D.create(1, 0, 1)
+    # sketchPt = sketchPts.add(point)
+    # sketchCircles = sketch.sketchCurves.sketchCircles
+    # centerPoint = adsk.core.Point3D.create(0, 0, 0)
+    # circle = sketchCircles.addByCenterRadius(centerPoint, 5.0)
+
+    # # Get the profile defined by the circle
+    # prof = sketch.profiles.item(0)
+
+    # # Create an extrusion input and make sure it's in a new component
+    # extrudes = rootComp.features.extrudeFeatures
+    # extInput = extrudes.createInput(prof, adsk.fusion.FeatureOperations.NewComponentFeatureOperation)
+
+    # # Set the extrusion input
+    # distance = adsk.core.ValueInput.createByReal(5)
+    # extInput.setDistanceExtent(True, distance)
+    # extInput.isSolid = True
+
+    # # Create the extrusion
+    # ext = extrudes.add(extInput)
+
+    # # Get the end face of the created extrusion body
+    # endFace = ext.endFaces.item(0)
+
+    # sketchInOcc = sketches.add(endFace, occ)
+    # curve = sketchInOcc.sketchCurves.item(0)
+
+    # geo0 = adsk.fusion.JointGeometry.createByCurve(curve, adsk.fusion.JointKeyPointTypes.CenterKeyPoint)
+
+    # # Create the second joint geometry with sketch point
+    # geo1 = adsk.fusion.JointGeometry.createByPoint(sketchPt)
+
+
+    # joints = occ.component.joints
+    # jointInput = joints.createInput(geo0, geo1)
+    # jointInput.setAsPlanarJointMotion(adsk.fusion.JointDirections.YAxisJointDirection)
+    # joint = joints.add(jointInput)
 
     # planarMotion = joint.jointMotion
     # limits = planarMotion.rotationLimits
